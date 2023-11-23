7258a75277298ca427479defa6d773798eae93f4
Manuel Del Castillo <manuel.delcastillo@walmeric.com>
2023-11-23 16:21:53 +0100

    testing integration serverApp

---
+++ src/presentation/server-app.test.ts
@@ -0,0 +1,43 @@
+import { CreateTable } from '../domain/use-cases/create-table.use-case'
+import { SaveFile } from '../domain/use-cases/save-file.use-case'
+import { ServerApp } from './server-app'
+
+describe('Server App', () => {
+  test('should create a ServerApp instance', () => {
+    const serverApp = new ServerApp()
+    expect(serverApp).toBeInstanceOf(ServerApp)
+    expect( typeof ServerApp.run ).toBe('function')
+  })
+  test('should run ServerApp with options', () => {
+
+    const logSpy = jest.spyOn(console, 'log')
+    const createTableSpy = jest.spyOn( CreateTable.prototype, 'execute' )
+    const saveFileSpy = jest.spyOn( SaveFile.prototype, 'execute' )
+
+    const options = {
+      base: 2,
+      limit: 10,
+      showTable: false,
+      fileDestination: 'test-destination',
+      fileName: 'test-filename'
+    }
+
+    ServerApp.run(options)
+
+    expect( logSpy ).toHaveBeenCalledTimes(2)
+    expect( logSpy ).toHaveBeenCalledWith('Server running...')
+    expect( logSpy ).toHaveBeenLastCalledWith('File created!')
+
+    expect( createTableSpy ).toHaveBeenCalledTimes(1)
+    expect( createTableSpy ).toHaveBeenCalledWith({
+      base: options.base, limit: options.limit
+    })
+
+    expect( saveFileSpy ).toHaveBeenCalledTimes(1)
+    expect( saveFileSpy ).toHaveBeenCalledWith({
+      fileContent: expect.any(String),
+      fileDestination: options.fileDestination,
+      fileName: options.fileName
+    })
+  })
+})
